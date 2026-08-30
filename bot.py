ccbc75204046bb3dad5fc6d834516843312027b5
afroghi786-coder <a.froghi786@gmail.com>
2026-08-30 13:43:39 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -253,7 +253,7 @@ ADMIN_CHAT_ID = "b0HWCJJ0xHE0e4e078b6c5228504866a"
 # 📊 تنظیمات گوگل‌شیت (Webhook)
 # ============================================================
 
-WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbyMQ5V5iwHWxzANM8sq5dfgQEHx5__NOyJmVvq6hULLbUOo2IdI-Y7wkmdjDi_m2SeJBA/exec"
+WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbywyrp5cXnKvLz2gRStDymuwruV1DMnG6VSF56H9RhCghjIhdfVkbmf77ZkMvT-CKDXLQ/exec"
 
 def ثبت_سفارش_در_شیت(customer, items, total, invoice_number, customer_code):
     try:
