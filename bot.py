64e6beef2367b9e5a9fafd63641088b9af65fb8d
afroghi786-coder <a.froghi786@gmail.com>
2026-09-04 16:42:40 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -155,7 +155,7 @@ for p in all_products:
 TOKEN = os.environ.get("TOKEN", "")
 BOT_USERNAME = "FroghiShopBot"
 ADMIN_CHAT_ID = "b0HWCJJ0xHE0e4e078b6c5228504866a"
-WEBHOOK_URL = "https://script.google.com/macros/s/AKfycby5BULzyCfvF3CtW4qM6qBJuSchMZVVB6LfJEY1dvr8moFFXJVuRivE-j6tEOVqr9D-Cw/exec"
+WEBHOOK_URL = "https://script.google.com/macros/s/AKfycby8J4Zi9s0GKM_-B8E8HKYDPwR9pEQAfOsk36FuL7cqY5vtATV8hMuepgvujwW8NgGTtQ/exec"
 
 # ============================================================
 # 🔍 توابع کمکی (بدون تغییر)
